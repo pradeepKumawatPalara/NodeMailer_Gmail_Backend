fb51b7dc237b966834f54980ee5a46f7a45da819
pradeep <pradeeppalara@gmail.com>
2023-05-28 13:27:32 +0530

    initial commit

--- controller/appController.js
+++ controller/appController.js
@@ -4,12 +4,12 @@ const Mailgen = require('mailgen');
 const { EMAIL, PASSWORD } = require('../env.js')
 
 /** send mail from testing account */
-const signup = async (req, res) => {
+const signup = async(req, res) => {
 
     /** testing account */
     let testAccount = await nodemailer.createTestAccount();
 
-      // create reusable transporter object using the default SMTP transport
+    // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: "smtp.ethereal.email",
         port: 587,
@@ -26,16 +26,16 @@ const signup = async (req, res) => {
         subject: "Hello ✔", // Subject line
         text: "Successfully Register with us.", // plain text body
         html: "<b>Successfully Register with us.</b>", // html body
-      }
+    }
 
 
     transporter.sendMail(message).then((info) => {
         return res.status(201)
-        .json({ 
-            msg: "you should receive an email",
-            info : info.messageId,
-            preview: nodemailer.getTestMessageUrl(info)
-        })
+            .json({
+                msg: "you should receive an email",
+                info: info.messageId,
+                preview: nodemailer.getTestMessageUrl(info)
+            })
     }).catch(error => {
         return res.status(500).json({ error })
     })
@@ -49,8 +49,8 @@ const getbill = (req, res) => {
     const { userEmail } = req.body;
 
     let config = {
-        service : 'gmail',
-        auth : {
+        service: 'gmail',
+        auth: {
             user: EMAIL,
             pass: PASSWORD
         }
@@ -60,35 +60,34 @@ const getbill = (req, res) => {
 
     let MailGenerator = new Mailgen({
         theme: "default",
-        product : {
-            name: "Mailgen",
-            link : 'https://mailgen.js/'
+        product: {
+            name: "BookMySchedule Team",
+            link: 'https://mailgen.js/'
         }
     })
 
     let response = {
         body: {
-            name : "Daily Tuition",
-            intro: "Your bill has arrived!",
-            table : {
-                data : [
-                    {
-                        item : "Nodemailer Stack Book",
-                        description: "A Backend application",
-                        price : "$10.99",
-                    }
-                ]
+            name: "user",
+            intro: "Your schedule has been confirmed!",
+            table: {
+                data: [{
+                    shopname: "krni",
+                    address: "jaipur",
+                    time: "4 pm",
+                }]
             },
-            outro: "Looking forward to do more business"
+            outro: "Looking forward you at shop"
         }
     }
 
     let mail = MailGenerator.generate(response)
 
     let message = {
-        from : EMAIL,
-        to : userEmail,
+        from: EMAIL,
+        to: userEmail,
         subject: "Place Order",
+        text: "trying",
         html: mail
     }
 
